8c9bf9083f0f99f6ce86f5c9b665401ea654441b
Jay <roebuck.jayc@gmail.com>
2022-04-11 17:32:04 -0400

    Added GraphQL view

--- src/jsuite/jsuite/urls.py
+++ src/jsuite/jsuite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from graphene_django.views import GraphQLView
+from jtext.schema import schema
+
 urlpatterns = [
     path('text/', include('jtext.urls')),
     path('admin/', admin.site.urls),
+    path("graphql/", GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
