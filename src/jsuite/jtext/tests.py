fb9ea831957617ad9883d00b77e8225ca21d7709
Jay <roebuck.jayc@gmail.com>
2022-04-11 15:58:32 -0400

    Added basic test for CI

--- src/jsuite/jtext/tests.py
+++ src/jsuite/jtext/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestUrls(TestCase):
+    app_url = "/text/"  # This is the path to the urls in this app (same as jsuite.urls with leading '/')
+
+    def test_jtext_index_url(self):
+        url = self.app_url + ''  # This is the relative url in this app (same as jtext.urls)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
