e5a81053e0c04850b717aca0f85e19ec006a60dd
Jay <roebuck.jayc@gmail.com>
2022-04-11 17:33:44 -0400

    Added test to landing page (404 for now)

--- src/jsuite/jsuite/tests.py
+++ src/jsuite/jsuite/tests.py
@@ -6,6 +6,12 @@ import graphene
 
 class TestUrls(TestCase):
 
+    # TODO Add a redirect in landing page so that there is no 404
+    def test_jsuite_index_url(self):
+        url = ""
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 404)
+
     def test_jsuite_graphql_url(self):
         # TODO Create a randomizer for graphql queries
         query = """
