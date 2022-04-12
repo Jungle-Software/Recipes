f7c190c516d12db2841d268438e863c5f99777a9
Jay <roebuck.jayc@gmail.com>
2022-04-11 21:12:37 -0400

    Switched base view of jtext to template view

--- src/jsuite/jtext/views.py
+++ src/jsuite/jtext/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello world :^),  this is the index of JText.")
+    context = {
+
+    }
+    return render(request, 'jtext/index.html', context)
