8de03ec8cef7d9cfe6df77b8a1b62f3b094fb922
Jay <roebuck.jayc@gmail.com>
2022-05-29 13:30:32 -0400

    modified queries for recipes to return required data for recipe view

--- jsuite/jtext/schema/queries.py
+++ jsuite/jtext/schema/queries.py
@@ -5,8 +5,18 @@ from .types import RecipeType
 
 
 class Query(graphene.ObjectType):
-    recipes = graphene.List(RecipeType)
+    all_recipes = graphene.List(RecipeType)
+    recipe_by_id = graphene.Field(RecipeType, id=graphene.Int())
 
-    def resolve_recipes(root, info, **kwargs):
-        # Querying a list
+    # Querying a list
+    def resolve_all_recipes(root, info, **kwargs):
         return Recipe.objects.all()
+
+    # Querying a single recipe
+    def resolve_recipe_by_id(root, info, id):
+        # In the front-end, we use default id = 0 until the user selects a recipe
+        if id == 0:
+            return None
+        return Recipe.objects.get(pk=id)
+
+
