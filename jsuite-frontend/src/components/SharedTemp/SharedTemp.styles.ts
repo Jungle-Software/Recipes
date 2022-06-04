f98890e57b0bb15777229bff735d33f6aafc5e09
Jay <roebuck.jayc@gmail.com>
2022-06-04 10:52:08 -0400

    added bootstrap to project

--- jsuite-frontend/src/components/SharedTemp/SharedTemp.styles.ts
+++ jsuite-frontend/src/components/SharedTemp/SharedTemp.styles.ts
@@ -1,6 +1,11 @@
 import styled from "styled-components";
+import { Button } from "react-bootstrap";
 
 export const Title = styled.h1`
   text-align: center;
   color: palevioletred;
+`;
+
+export const BootstrapButton = styled(Button)`
+  background: palevioletred;
 `;
