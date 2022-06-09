92bd4c1b836c66e2946d6b09082db48878d7c20f
Jay <roebuck.jayc@gmail.com>
2022-06-09 19:11:32 +0000

    installed and configured cypress for api and e2e testing

---
+++ jsuite-frontend/cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    specPattern: 'cypress/{e2e,api}/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:3000',
+    env: {
+      apiUrl: 'http://localhost:8080/graphql',
+  
+    },
+  },
+});
