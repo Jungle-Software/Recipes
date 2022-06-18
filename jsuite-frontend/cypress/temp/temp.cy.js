5bcbc62511beefbd32fb787d5dc93f83b2f4d031
Jay <roebuck.jayc@gmail.com>
2022-06-18 10:44:21 -0400

    adding get by id to now passing cypress test

--- jsuite-frontend/cypress/temp/temp.cy.js
+++ jsuite-frontend/cypress/temp/temp.cy.js
@@ -1,5 +1,6 @@
 describe('recipe page', () => {
     it('should pass i stg', () => {
         cy.visit('/')
+        cy.get('[id=shared-temp]').should('have.text', 'Bootstrap button :eyes:')
     })
 })
