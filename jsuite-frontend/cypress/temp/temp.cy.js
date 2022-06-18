019b576fb1482eb0a2b7cbcfbfc26daad2180450
Jay <roebuck.jayc@gmail.com>
2022-06-18 10:52:41 -0400

    adding intercept

--- jsuite-frontend/cypress/temp/temp.cy.js
+++ jsuite-frontend/cypress/temp/temp.cy.js
@@ -1,6 +1,24 @@
+import { hasOperationName } from '../utils/graphql-test-utils'
+
 describe('recipe page', () => {
-    it('should pass i stg', () => {
-        cy.visit('/')
-        cy.get('[id=shared-temp]').should('have.text', 'Bootstrap button :eyes:')
+    const recipeApiUrl = Cypress.env('apiUrl') + '/recipes';
+
+    beforeEach(() => {
+        cy.intercept('POST', recipeApiUrl, (req) => {
+            if (hasOperationName(req, 'AllRecipes')) {
+                req.alias = 'AllRecipesResponse'
+                req.reply({ fixture: 'Recipes/allRecipes'})
+            }
+
+            if (hasOperationName(req, 'RecipeById')) {
+                req.alias = 'RecipeByIdResponse'
+                req.reply({ fixture: 'Recipes/recipeById'})
+            }
+        })
+    })
+
+    it('prompts the user to select a recipe', () => {
+        cy.visit('/recipes')
+        cy.get('[id=select-recipe-prompt]').should('have.text', 'Please select a recipe (or insert a new one if you have none!)')
     })
 })
