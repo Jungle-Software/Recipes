0c8a6cf51fa856966a09e1cea48b906578599261
kelloggs522 <laguimauve522@gmail.com>
2022-07-10 19:54:19 -0400

    Modified relevant test

--- jsuite-frontend/src/components/NavBar/tests/NavBar.test.tsx
+++ jsuite-frontend/src/components/NavBar/tests/NavBar.test.tsx
@@ -2,11 +2,12 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import NavBar from '../NavBar';
 
-test('render navBar component', () => {
-    render(
+it('render navBar component', async () => {
+    const { findByText } = render(
         <MemoryRouter>
             <NavBar />
         </MemoryRouter>
     );
-
+    const navbarElement = await findByText("Jrecipe", { exact: false})
+    expect(navbarElement).toBeInTheDocument();
 });
