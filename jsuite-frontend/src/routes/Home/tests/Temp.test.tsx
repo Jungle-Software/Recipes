39e81ffc607b3296fbf6ad83809130c4f4ef58eb
Jay <roebuck.jayc@gmail.com>
2022-09-01 09:48:09 -0400

    refactored temp into home and removed unused components

--- jsuite-frontend/src/routes/Home/tests/Temp.test.tsx
+++ jsuite-frontend/src/routes/Home/tests/Temp.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import Temp from '../Temp';
+import Temp from '../Home';
 
 it('renders temp landing page link', async () => {
   const { findByText } = render(
