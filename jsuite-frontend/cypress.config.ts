9395d50fc134f1b7c3015f90ec561df3f357dea1
Jay <roebuck.jayc@gmail.com>
2023-01-02 22:41:12 -0500

    run cypress not on localhost

--- jsuite-frontend/cypress.config.ts
+++ jsuite-frontend/cypress.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/{e2e,}/**/*.cy.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://127.0.0.1:3000',
     env: {
-      apiUrl: 'http://localhost:8080/graphql',
+      apiUrl: 'http://127.0.0.1:8080/graphql',
   
     },
     video: false,
